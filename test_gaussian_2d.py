06799fb8dec6cc2438d8c058ebe96a5854a54cae
Emil Christiansen <emil.christiansen@ntnu.no>
2017-01-31 22:40:30 +0100

    Unit test for fittable gaussian
    Added a unit test method to test if gaussian_2d() is fittable using
    scipy.curve_fit()

--- test_gaussian_2d.py
+++ test_gaussian_2d.py
@@ -1,6 +1,7 @@
 from sped_align import gaussian_2d
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 def test_gaussian_2d_size():
@@ -25,3 +26,18 @@ def test_gaussian_2d_center():
                 int(round(xo, ndigits=0)), int(round(yo, ndigits=0))), print(
                 'AssertionError on testing gaussian 2d center:\n\tmax at: {0} should be at {1}\n{2}'.format(
                     np.unravel_index(np.argmax(g), (n, m)), (xo, yo), g))
+
+def test_gaussian_2d_fittable():
+    n = 100
+    m = 100
+    xo = 20
+    yo = 10
+    x, y = np.mgrid[0:n, 0:m]
+    a = 1000.0
+    g = gaussian_2d((x, y), a, xo, yo, 5, 5, 0, 0).reshape(np.shape(x))
+    try:
+        popt, pcov = curve_fit(gaussian_2d, (x, y), g.ravel(), p0=[a, xo, yo, 100, 100, 0, 0],
+                               bounds=([0.0, 0.0, 0.0, 0.0, 0.0, -45.0, 0.0],
+                                       [a * 10.0, float(n), float(m), np.inf, np.inf, 45.0, np.inf]))
+    except:
+        assert False
